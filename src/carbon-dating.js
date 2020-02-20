cceadfcbc25c4afd5502b9bb74f715b374112b86
Iuliia Plekhanova <julec2006@yandex.ru>
2020-02-20 22:40:13 +0400

    fix: add solutions to count-cats.js & carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
+module.exports = function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  if (typeof sampleActivity == "string") {
+    let n0 = parseFloat(sampleActivity);
+    if (!isNaN(n0) && n0 != undefined 
+    && n0 > 0 && n0 <= 15) {
+      return Math.ceil(Math.log(MODERN_ACTIVITY/n0)/0.693*HALF_LIFE_PERIOD);
+    } 
+    else return false;
+  } else return false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(matrix) {
   // remove line with error and write your code here
+  let result = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == '^^') result++;
+    }
+  }
+  return result;
 };
