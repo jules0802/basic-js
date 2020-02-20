6c7304f42f249148066304087c1c76225d897356
Iuliia Plekhanova <julec2006@yandex.ru>
2020-02-20 23:07:42 +0400

    fix: add solution to dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {  
   // remove line with error and write your code here
+  if (Array.isArray(members)) {
+    let result = "";
+    members = members.filter(item => typeof item == "string").map(item => item.trim().toUpperCase()).sort();
+    for (let item of members) {
+      result+=item[0];
+    }
+    return result;
+  } else return false;
 };
