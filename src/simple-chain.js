7b80ee791ffab78d74fd22858612239559aa9385
Iuliia Plekhanova <julec2006@yandex.ru>
2020-02-24 19:48:23 +0400

    fix: add solution to simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,39 @@
+
 const chainMaker = {
+  count: 0,
+  array: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = ++this.count;
+    count = 0; 
+    return result;
   },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+  addLink(value) {    
+    this.count++;
+    this.array.push(`( ${value} )`);
+    return this;
   },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+  removeLink(position) { 
+    this.count++;
+    if (position-1 < 0 || position >= this.array.length || typeof position !== 'number') { 
+      this.array=[];
+      throw new Error();      
+    }
+    this.count++;
+    this.array.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.count++;
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = this.array.join('~~');
+    this.array = [];
+    return result;
   }
 };
 
 module.exports = chainMaker;
+
+//console.log(chainMaker.addLink('8.963').reverseChain().reverseChain().reverseChain().reverseChain().addLink({0: 'first', 1: 'second', 'length': 2}).reverseChain().addLink(3.14).addLink('DEF').reverseChain().finishChain());
