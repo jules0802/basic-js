a8165ba2bc8e1a2eedab0b8c1b3d26f971e0d1de
Iuliia Plekhanova <julec2006@yandex.ru>
2020-02-25 23:32:00 +0400

    fix: add solution to recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,18 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        if (!arr.reduce((check, item) => check = check || Array.isArray(item),0)) {
+            return 1;
+        }
+        else {            
+            let maxDepth = 1;
+            for (let item of arr) {
+                if (Array.isArray(item)) {
+                    let depth = 1;
+                    depth += this.calculateDepth(item);
+                    if (depth > maxDepth) maxDepth = depth;
+                }
+            }
+            return maxDepth;
+        }
     }
 };
