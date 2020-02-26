2c08d44d666dd58dbd6937d88dcb3edc5d46837a
Iuliia Plekhanova <julec2006@yandex.ru>
2020-02-26 21:20:51 +0400

    fix: add solution to vigenere-cipher.js (not all tests)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,43 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor (machineType) {
+    this.machineType = machineType;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (message === undefined || key === undefined) throw new Error();
+            key = key.toUpperCase();
+            message = message.toUpperCase();
+            let result = '';
+            let countOfnotLetters = 0;
+            for (let i=0; i<message.length; i++) {
+                if (this.alphabet.indexOf(message[i])>=0) {
+                result += this.alphabet[(this.alphabet.indexOf(message[i])+this.alphabet.indexOf(key[(i-countOfnotLetters)%key.length]))%this.alphabet.length];
+                } else {
+                    result += message[i];
+                    countOfnotLetters++;
+                }
+            }           
+        return this.machineType === undefined || true? result: result.split('').reverse().join();       
+    }
+
+    decrypt(encryptedMessage, key) {
+        if (encryptedMessage === undefined || key === undefined) throw new Error();
+        //if (this.machineType == 'reverse') {
+            key = key.toUpperCase();
+            encryptedMessage = encryptedMessage.toUpperCase();
+            let result = '';
+            let countOfnotLetters = 0;
+            for (let i=0; i<encryptedMessage.length; i++) {
+                if (this.alphabet.indexOf(encryptedMessage[i])>=0) {
+                result += this.alphabet[(this.alphabet.indexOf(encryptedMessage[i])+this.alphabet.length-this.alphabet.indexOf(key[(i-countOfnotLetters)%key.length]))%this.alphabet.length];
+                } else {
+                    result += encryptedMessage[i];
+                    countOfnotLetters++;
+                }
+            }
+            return this.machineType === undefined || true? result: result.split('').reverse().join();  
+       // }
     }
 }
 
